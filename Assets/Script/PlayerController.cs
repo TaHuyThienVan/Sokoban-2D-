8c3dbbbe52b1a5d6b69160c68a858c8e281b5a5f
Imagine-Astronomy-THTV <tathienvan2000@gmail.com>
2025-12-05 23:58:59 +0700

    title map and push animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -24,26 +24,35 @@ public class PlayerController : MonoBehaviour
             TryMove(dir);
     }
 
-    void TryMove(Vector2 dir)
+    void TryMove(Vector2 direction)
     {
         Vector2 start = transform.position;
-        Vector2 target = start + dir;
+        Vector2 target = start + direction;
 
-        RaycastHit2D hit = Physics2D.Raycast(start, dir, 1f, blockingLayer);
+        RaycastHit2D hit = Physics2D.Raycast(start, direction, 1f, blockingLayer);
 
-        // N?u không có v?t c?n ? ?i luôn
         if (hit.collider == null)
         {
+            animator.SetBool("isRunning", true);
             StartCoroutine(MoveTo(target));
             return;
         }
 
-        // N?u g?p h?p ? th? ??y
         if (hit.collider.CompareTag("Box"))
         {
             BoxController box = hit.collider.GetComponent<BoxController>();
-            if (box.TryPush(dir))
+
+            if (box.TryPush(direction))  
+            {
+                animator.SetTrigger("Push");   
+                animator.SetBool("isRunning", false); 
                 StartCoroutine(MoveTo(target));
+            }
+            else
+            {
+          
+                animator.SetBool("isRunning", false);
+            }
         }
     }
 
@@ -51,19 +60,18 @@ public class PlayerController : MonoBehaviour
     {
         isMoving = true;
 
-        animator.SetBool("isRunning", true);   // ?? b?t animation
-
         while ((Vector2)transform.position != target)
         {
             transform.position = Vector2.MoveTowards(
-                transform.position,
-                target,
-                moveSpeed * Time.deltaTime
-            );
+                                                            transform.position,
+                                                            target,
+                                                            moveSpeed * Time.deltaTime
+                                                        );
+
             yield return null;
         }
 
-        animator.SetBool("isRunning", false);  // ?? t?t animation
+        animator.SetBool("isRunning", false); 
         isMoving = false;
     }
 }
